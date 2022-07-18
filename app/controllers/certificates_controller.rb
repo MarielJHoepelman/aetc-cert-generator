1690134c32e85d4e1a251d8c349259513e04d6d5
Mariel Hoepelman <marielhoepelman@gmail.com>
2022-07-18 14:21:23 -0400

    WIP

--- app/controllers/certificates_controller.rb
+++ app/controllers/certificates_controller.rb
@@ -2,6 +2,11 @@ class CertificatesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    binding.pry
+    Generator::Certificate.run(event_params)
+    render json: {}, status: :created
+  end
+
+  def event_params
+    params.require(:event).permit(:event_name, :event_date, :speaker, :preview, :participants => [])
   end
 end
--- lib/generator/certificate.rb
+++ lib/generator/certificate.rb
@@ -1,30 +1,17 @@
-require 'csv'
-
 module Generator
   module Certificate
     module_function
 
-    def parse_csv
-      csv = File.read('lib/generator/data/input.csv')
-      CSV.parse(csv, headers: false).map do |row|
-        row.join()
+    def run(event_params)
+      if event_params[:preview]
+        event_params[:participants] = event_params[:participants][0..0]
+      end
+      event_params[:participants].each do |name|
+        create_image(name,event_params[:event_name], event_params[:event_date].to_date.to_formatted_s(:long), event_params[:speaker])
       end
     end
 
-    def run
-      # name = "Jie Jiang, RD, CDN"
-      event_title = "New Formulations for HIV Prevention and Treatment"
-      # event_sub = "More Culturally Responsive Towards Transgender Patients"
-      speaker = "Antonio Urbina, MD"
-      date = "April 7, 2022"
-
-      parse_csv.each do |name|
-        create_image(name, event_title, date, speaker)
-      end
-
-    end
-
-    def create_image(name, event_title, date, speaker)
+    def create_image(name, event_name, event_date, speaker)
       image = MiniMagick::Image.open("lib/generator/images/certificate.pdf")
       image.combine_options do |c|
         c.density 150
@@ -35,16 +22,17 @@ module Generator
         c.gravity "center"
         c.draw "text 0, -40 '#{name}'"
         c.pointsize 22
-        c.draw "text 0, 105 '#{event_title}'"
+        c.draw "text 0, 105 '#{event_name}'"
         # c.pointsize 18
         # c.draw "text 0, 135 '#{event_sub}'"
         c.pointsize 18
-        c.draw "text 0, 160 '#{date}'"
+        c.draw "text 0, 160 '#{event_date}'"
         c.pointsize 20
         c.draw "text 0, 260 '#{speaker}'"
       end
 
       image.write("lib/generator/output/#{name.parameterize}.pdf")
+      binding.pry
     end
   end
 end
